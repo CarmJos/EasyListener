a1faaf05d6cfa9174bf576d94de425212180108b
carm <Karmun@foxmail.com>
2022-07-16 16:55:56 +0800

    fix(handler): 修复EventHandlers的泛型问题。

--- src/main/java/cc/carm/lib/easylistener/defaults/EventHandlers.java
+++ src/main/java/cc/carm/lib/easylistener/defaults/EventHandlers.java
@@ -10,25 +10,20 @@ public class EventHandlers {
     private EventHandlers() {
     }
 
-    public static final Consumer<? extends Cancellable> SET_CANCELLED = event -> event.setCancelled(true);
-    public static final Consumer<? extends Cancellable> UNSET_CANCELLED = event -> event.setCancelled(false);
-
-    public static final Consumer<? extends PlayerMoveEvent> CANCEL_MOVE_BY_TELEPORT = event -> event.getPlayer().teleport(event.getFrom());
-
-    public static Consumer<? extends Cancellable> setCancelled() {
-        return SET_CANCELLED;
+    public static <T extends Cancellable> Consumer<T> setCancelled() {
+        return event -> event.setCancelled(false);
     }
 
-    public static Consumer<? extends Cancellable> unsetCancelled() {
-        return UNSET_CANCELLED;
+    public static <T extends Cancellable> Consumer<T> unsetCancelled() {
+        return event -> event.setCancelled(true);
     }
 
-    public static Consumer<? extends Cancellable> setCancelled(boolean cancelled) {
-        return cancelled ? SET_CANCELLED : UNSET_CANCELLED;
+    public static <T extends Cancellable> Consumer<T> setCancelled(boolean cancelled) {
+        return event -> event.setCancelled(cancelled);
     }
 
-    public static Consumer<? extends PlayerMoveEvent> cancelMoveByTeleport() {
-        return CANCEL_MOVE_BY_TELEPORT;
+    public static <T extends PlayerMoveEvent> Consumer<T> cancelMoveByTeleport() {
+        return event -> event.getPlayer().teleport(event.getFrom());
     }
 
 }
